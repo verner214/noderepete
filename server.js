8a2d312549b9430bb990f899611ee1af9afc3792
verner214 <lars.waernulf@gmail.com>
2017-02-08 15:00:27 +0100

    rensat lite iallafall

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+//lognew finns i olapp, använd den istället
 /* node server.js
   webbserver som till�ter uppladdning av filer till azure storage för olapp
   dessa environment variabler m�ste s�ttas innan servern startas:
 set AZURE_STORAGE_ACCOUNT=portalvhdsgfh152bhy290k
 set AZURE_STORAGE_ACCESS_KEY=[key from azure] ---se appsettings.bat, checkas inte in i git
   
+obs! env.PORT används också
+
 obs! bra l�nk som �r r�tt och inte fel!, http://azure.microsoft.com/sv-se/develop/nodejs/
 obs! om hur man laddar upp och skapar fil innan, http://stackoverflow.com/questions/18317904/stream-uploaded-file-to-azure-blob-storage-with-node
 ocks� en l�nk som har r�tt information, http://azure.microsoft.com/sv-se/documentation/articles/storage-nodejs-how-to-use-table-storage/
@@ -18,11 +21,8 @@ var uuid = require('node-uuid');
 var async = require("async");
 
 var app = express();
-var containerName = "cntolapp";
-//var tableName = "tblolapp";
-var tblOlapp = "tblolapp";
-var tblDemo = "tblolappdemo";
-//var tableNameDemo = "tblolappdemo";
+var AZURE_CONTAINER = "cntolapp";
+var AZURE_TABLE = "tblrepete";
 var AZURE_STORAGE_ACCOUNT = "portalvhdsgfh152bhy290k";
 var AZURE_STORAGE_ACCESS_KEY = process.env.AZURE_STORAGE_ACCESS_KEY;
 var hostName = "https://" + AZURE_STORAGE_ACCOUNT + ".blob.core.windows.net";
@@ -51,10 +51,10 @@ var saveImages = function(files, callback) {
 
 //sparar en BLOB som ligger p� disk. callback(err, url) meddelas urlen som bloben fick.
     var saveBLOB = function (filePath, callback) {
-        blobService.createBlockBlobFromLocalFile(containerName, filePath, filePath, function (err, result) {
+        blobService.createBlockBlobFromLocalFile(AZURE_CONTAINER, filePath, filePath, function (err, result) {
             if (err) return callback(err);
             console.log('fil SPARAD till BLOB, fil=' + filePath);
-            var imgUrl = blobService.getUrl(containerName, filePath, null, hostName);
+            var imgUrl = blobService.getUrl(AZURE_CONTAINER, filePath, null, hostName);
             callback(null, { url: imgUrl, name: filePath });
         });
     }
@@ -99,7 +99,6 @@ app.post('/newedit', function (req, res, next) {
     form.keepExtensions = true;     //keep file extension
 
     form.parse(req, function(err, fields, files) {
-        var tableName = (fields.demo === undefined || fields.demo.localeCompare("false") === 0) ? tblOlapp : tblDemo;
 
 //debug
         console.log("newedit, id = "  + fields.id);
@@ -110,7 +109,7 @@ app.post('/newedit', function (req, res, next) {
 //nytt: spara ev. bilder till blob, hantera skapade urler
         saveImages(files, function(img, thumb) {
             var tableSvc = azure.createTableService(AZURE_STORAGE_ACCOUNT, AZURE_STORAGE_ACCESS_KEY);
-            tableSvc.createTableIfNotExists(tableName, function (err, result, response) {
+            tableSvc.createTableIfNotExists(AZURE_TABLE, function (err, result, response) {
                 if (err) throw err;
                 var entGen = azure.TableUtilities.entityGenerator;
                 var task = {
@@ -136,7 +135,7 @@ app.post('/newedit', function (req, res, next) {
                     task.thumbName = entGen.String(thumb.name);
                 }
                 if (bNew) {
-                    tableSvc.insertEntity(tableName, task, function (err, result, response) {
+                    tableSvc.insertEntity(AZURE_TABLE, task, function (err, result, response) {
                         if (err) throw err;
                         console.log("insert");
                         //var fullUrl = req.protocol + '://' + req.get('host');
@@ -144,7 +143,7 @@ app.post('/newedit', function (req, res, next) {
                         res.send('OK');
                     });
                 } else {
-                    tableSvc.mergeEntity(tableName, task, function (err, result, response) {
+                    tableSvc.mergeEntity(AZURE_TABLE, task, function (err, result, response) {
                         if (err) throw err;
                         console.log("update");
                         //efter post, visa list.html        
@@ -157,132 +156,6 @@ app.post('/newedit', function (req, res, next) {
         });//saveImages
     });//form.parse
 });//app.post('/newedit'
-
-//gallerynew, param: id (rowid), thumb, img, text (optional) 
-app.post('/gallerynew', function (req, res, next) {
-    var form = new formidable.IncomingForm();
-    form.uploadDir = uploadDir;       //set upload directory, Formidable uploads to operating systems tmp dir by default
-    form.keepExtensions = true;     //keep file extension
-
-    form.parse(req, function(err, fields, files) {
-        var tableName = (fields.demo === undefined || fields.demo.localeCompare("false") === 0) ? tblOlapp : tblDemo;
-        
-        if (err) throw err;
-        console.log("gallerynew, id = "  + fields.id);
-        
-//nytt: spara ev. bilder till blob, hantera skapade urler
-        saveImages(files, function(img, thumb) {
-            var tableSvc = azure.createTableService(AZURE_STORAGE_ACCOUNT, AZURE_STORAGE_ACCESS_KEY);
-            tableSvc.retrieveEntity(tableName, partitionKey, fields.id, function(err, result, response) {
-                if (err) throw err;
-                var galleryArr = [];
-                if (result["gallery"] !== undefined) {
-                    galleryArr = JSON.parse(result["gallery"]["_"]);
-                }
-                var galleryItem = {imgURL: img.url, thumbURL: thumb.url, text: fields.text, hide: false};
-                galleryArr.push(galleryItem);
-                
-//merga med existing row                
-                var entGen = azure.TableUtilities.entityGenerator;
-                var task = {
-                    PartitionKey: entGen.String(partitionKey),
-                    RowKey: entGen.String(fields.id),
-                    gallery: entGen.String(JSON.stringify(galleryArr))
-                };                
-                tableSvc.mergeEntity(tableName, task, function (err, result, response) {
-                    if (err) throw err;
-                    console.log("mergeEntity");
-                    res.send('OK');
-                });
-            });//tableSvc.retrieveEntity
-        });//saveImages
-    });//form.parse
-});//app.post('/gallerynew'
-
-//galleryedit, param: id (rowid), imgURL (för identifiering), text (optional), hide (optional) 
-app.post('/galleryedit', function (req, res, next) {
-    var form = new formidable.IncomingForm();
-    form.uploadDir = uploadDir;       //set upload directory, Formidable uploads to operating systems tmp dir by default
-    form.keepExtensions = true;     //keep file extension
-
-    form.parse(req, function(err, fields, files) {
-        var tableName = (fields.demo === undefined || fields.demo.localeCompare("false") === 0) ? tblOlapp : tblDemo;
-        
-        if (err) throw err;
-        console.log("galleryedit, id = "  + fields.id);
-        
-//hämta brygd
-        var tableSvc = azure.createTableService(AZURE_STORAGE_ACCOUNT, AZURE_STORAGE_ACCESS_KEY);
-        tableSvc.retrieveEntity(tableName, partitionKey, fields.id, function(err, result, response) {
-            if (err) throw err;
-            var galleryArr = JSON.parse(result["gallery"]["_"]);
-            //var galleryItem = {imgURL: img.url, thumbURL: thumb.url, text: fields.text, hide: false};
-            for (var n = 0; n < galleryArr.length; n++) {
-                if (fields.imgURL == galleryArr[n].imgURL) {
-                    galleryArr[n].hide = (fields.hide == "true" || fields.hide == "on") ? "true" : "false";
-                    galleryArr[n].text = fields.text;
-                }
-            }
-                
-//merga med existing row                
-            var entGen = azure.TableUtilities.entityGenerator;
-            var task = {
-                PartitionKey: entGen.String(partitionKey),
-                RowKey: entGen.String(fields.id),
-                gallery: entGen.String(JSON.stringify(galleryArr))
-            };                
-            tableSvc.mergeEntity(tableName, task, function (err, result, response) {
-                if (err) throw err;
-                console.log("mergeEntity");
-                res.send('OK');
-            });
-        });//tableSvc.retrieveEntity
-    });//form.parse
-});//app.post('/gallerynew'
-
-//för att kopiera enstaka rad till demotabellen (azure-explorer kan ta bort rader men inte kopiera)
-app.post('/copy2demo', function (req, res, next) {
-    console.log("copy2demo, enter");
-    var form = new formidable.IncomingForm();
-
-    form.parse(req, function(err, fields, files) {
-        var tableSvc = azure.createTableService(AZURE_STORAGE_ACCOUNT, AZURE_STORAGE_ACCESS_KEY);
-        tableSvc.retrieveEntity(tblOlapp, partitionKey, fields.id, function(err, result, response) {
-            if (err) throw err;
-            tableSvc.insertEntity(tblDemo, result, function (err, result2, response) {
-                if (err) throw err;
-                console.log("insert till demo gjord");
-                res.send('OK');
-            });
-        });//tableSvc.retrieveEntity
-    });//form.parse
-});//copy2demo
-
-//log vid fel och appstart
-app.post('/lognew', function (req, res, next) {
-    console.log("lognew, enter");
-    var form = new formidable.IncomingForm();
-    var logTableName = "lawalogtable";
-    console.log("lognew, enter2");
-
-    form.parse(req, function(err, fields, files) {
-        var tableSvc = azure.createTableService(AZURE_STORAGE_ACCOUNT, AZURE_STORAGE_ACCESS_KEY);
-        tableSvc.createTableIfNotExists(logTableName, function (err, result, response) {
-            var entGen = azure.TableUtilities.entityGenerator;
-            var task = {
-                PartitionKey: entGen.String(partitionKey),//obligatorisk
-                RowKey: entGen.String(uuid()),//obligatorisk
-                device: entGen.String(fields.device),
-                error: entGen.String(fields.error)
-            };
-            tableSvc.insertEntity(logTableName, task, function (err, result, response) {
-                if (err) throw err;
-                console.log("insert");
-                res.send('OK');
-            });
-        });//tableSvc.createTableIfNotExists
-    });//form.parse
-});//lognew
 
 app.get('/sas', function (req, res) {
 	var startDate = new Date();
@@ -298,10 +171,8 @@ app.get('/sas', function (req, res) {
 		},
 	};
 	var tableSvc = azure.createTableService(AZURE_STORAGE_ACCOUNT, AZURE_STORAGE_ACCESS_KEY);
-	var tableSAS = tableSvc.generateSharedAccessSignature(tblOlapp, sharedAccessPolicy);
+	var tableSAS = tableSvc.generateSharedAccessSignature(AZURE_TABLE, sharedAccessPolicy);
 	var html = "<b>SAS</b>" + tableSAS + "<br>" + "<b>host</b><br>" + JSON.stringify(tableSvc.host) + "<br>";
-	tableSAS = tableSvc.generateSharedAccessSignature(tblDemo, sharedAccessPolicy);
-	html += "<b>SAS-demo</b>" + tableSAS + "<br>" + "<b>host</b><br>" + JSON.stringify(tableSvc.host) + "<br>";
 	res.send(html);
 	//https://portalvhdsgfh152bhy290k.table.core.windows.net/photos?st=2015-03-28T20%3A41%3A10Z&se=2015-03-29T00%3A01%3A10Z&sp=r&sv=2014-02-14&tn=photos&sig=yf8MoYRO8kAO4NF89krvZDLjLycVgOBHA%2FC%2FCIc0vV0%3D
 });
