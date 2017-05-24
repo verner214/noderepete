6af7e3973754ca699e0bdb477849ba466df49524
verner214 <lars.waernulf+5@gmail.com>
2017-05-24 21:02:10 +0200

    filter hide on

--- public/own.js
+++ public/own.js
@@ -4,3 +4,7 @@ function getParameterByName(name) {
         results = regex.exec(location.search);
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
+
+function last4(str) {
+    return (str.substring(str.length - 4, str.length));
+}
