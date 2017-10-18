60b43255f21a9213bdacbcbb3431fade21851f75
verner214 <lars.waernulf@gmail.com>
2017-10-18 12:54:00 +0200

    htmlencode

--- public/own.js
+++ public/own.js
@@ -67,3 +67,14 @@ function orderArray(arr) {
     }
     return orderArr;
 }
+
+function htmlEncode(value){
+  //create a in-memory div, set it's inner text(which jQuery automatically encodes)
+  //then grab the encoded contents back out.  The div never exists on the page.
+  return $('<div/>').text(value).html();
+}
+
+function htmlDecode(value){
+  return $('<div/>').html(value).text();
+}
+
